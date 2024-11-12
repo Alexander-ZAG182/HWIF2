1935dbf405727460db69c4b2b9ed0b5f5fc4fc3b
Alexander-ZAG182 <hado18190@gmail.com>
2024-11-13 01:04:43 +0300

    halfLesson4

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println( "Задача 1");
+        System.out.println("Задача 1");
 
         int clientOS = 1;
-        if (clientOS == 1) {System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0){System.out.println("Установите версию приложения для iOS по ссылке");
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2014;
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else System.out.println("Неизвестная ос");
+
+        System.out.println("Задача 3");
+        int year = 2004;
+        if (year > 1584) {
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
+        } else {
+            System.out.println("Год не может быть меньше 1585");
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Задача 4");
 
 
 
